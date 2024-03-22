428ab80c9d62f7973fdd499bf1b2ef85cff89d4c
Satoshi550 <78008562+Satoshi550@users.noreply.github.com>
2024-03-22 09:30:31 -0500

    Initial Commit

---
+++ ConstructionTimer V1.3/ConstructionTimer_V1_3App.swift
@@ -0,0 +1,17 @@
+//
+//  ConstructionTimer_V1_3App.swift
+//  ConstructionTimer V1.3
+//
+//  Created by Satoshi Mitsumori on 3/22/24.
+//
+
+import SwiftUI
+
+@main
+struct ConstructionTimer_V1_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConstructionTimer V1.3UITests/ConstructionTimer_V1_3UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ConstructionTimer_V1_3UITestsLaunchTests.swift
+//  ConstructionTimer V1.3UITests
+//
+//  Created by Satoshi Mitsumori on 3/22/24.
+//
+
+import XCTest
+
+final class ConstructionTimer_V1_3UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
